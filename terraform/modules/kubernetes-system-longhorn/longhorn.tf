969490159ddf3f6028c969fcc51979b390b688fc
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-30 00:20:18 +0100

    use manifest directly to avoild api calls

--- terraform/modules/kubernetes-system-longhorn/longhorn.tf
+++ terraform/modules/kubernetes-system-longhorn/longhorn.tf
@@ -24,16 +24,44 @@ resource "helm_release" "longhorn" {
   ]
 }
 
-data "kubectl_path_documents" "longhorn" {
-  pattern = "${path.module}/kubernetes/longhorn/*.yaml"
-  vars = {
-      namespace = "longhorn"
-  }
-}
-
 resource "kubectl_manifest" "longhorn" {
-  for_each  = toset(data.kubectl_path_documents.longhorn.documents)
-  yaml_body = each.value
+  yaml_body  = <<-EOF
+---
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
+  name: longhorn-dashboard
+  namespace: longhorn-system
+spec:
+  entryPoints:
+    - websecure
+  routes:
+    - match: Host(`longhorn.zelos.k8s.infra.erpf.de`) # Hostname to match
+      kind: Rule
+      services: # Service to redirect requests to
+        - name: longhorn-frontend
+          port: 80
+      # middlewares:
+      #   - name: longhorn-basic-auth
+  tls:
+    secretName: longhorn-erpf-de-tls
+    
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: longhorn-erpf-de-tls
+  namespace: longhorn-system
+spec:
+  secretName: longhorn-erpf-de-tls
+  issuerRef:
+    name: cloudflare-letsencrypt-prod
+    kind: ClusterIssuer
+  commonName: longhorn.zelos.k8s.infra.erpf.de
+  dnsNames:
+  - "longhorn.zelos.k8s.infra.erpf.de"
+  EOF
+
   depends_on = [
     kubernetes_namespace.longhorn,
     helm_release.longhorn
--- terraform/modules/kubernetes-system-monitoring/kube-prometheus-stack.tf
+++ terraform/modules/kubernetes-system-monitoring/kube-prometheus-stack.tf
@@ -22,16 +22,40 @@ resource "helm_release" "monitoring" {
   ]
 }
 
-data "kubectl_path_documents" "monitoring" {
-  pattern = "${path.module}/kubernetes/*.yaml"
-  vars = {
-      namespace = "monitoring"
-  }
-}
-
 resource "kubectl_manifest" "monitoring" {
-  for_each  = toset(data.kubectl_path_documents.monitoring.documents)
-  yaml_body = each.value
+  yaml_body  = <<-EOF
+---
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
+  name: grafana
+spec:
+  entryPoints:
+    - websecure
+  routes:
+    - match: Host(`grafana.zelos.k8s.infra.erpf.de`) # Hostname to match
+      kind: Rule
+      services: # Service to redirect requests to
+        - name: monitoring-grafana
+          port: 80
+  tls:
+    secretName: grafana-erpf-de-tls
+    
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: grafana-erpf-de-tls
+spec:
+  secretName: grafana-erpf-de-tls
+  issuerRef:
+    name: cloudflare-letsencrypt-prod
+    kind: ClusterIssuer
+  commonName: grafana.zelos.k8s.infra.erpf.de
+  dnsNames:
+  - "grafana.zelos.k8s.infra.erpf.de"
+  EOF
+
   depends_on = [
     kubernetes_namespace.monitoring,
     helm_release.monitoring

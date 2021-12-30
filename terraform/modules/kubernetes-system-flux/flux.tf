490d015c53c331635cffe34e048c0f2995385168
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-30 02:59:28 +0100

    fix flux module complete

--- terraform/modules/kubernetes-system-flux/flux.tf
+++ terraform/modules/kubernetes-system-flux/flux.tf
@@ -43,39 +43,39 @@ locals {
   ]
 }
 
-# resource "kubectl_manifest" "install" {
-#   for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-#   depends_on = [
-#     kubernetes_namespace.flux_system
-#   ]
-#   yaml_body  = each.value
-# }
+resource "kubectl_manifest" "install" {
+  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [
+    kubernetes_namespace.flux_system
+  ]
+  yaml_body  = each.value
+}
 
-# resource "kubectl_manifest" "sync" {
-#   for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-#   depends_on = [
-#     kubernetes_namespace.flux_system,
-#     kubectl_manifest.install
-#   ]
-#   yaml_body  = each.value
-# }
+resource "kubectl_manifest" "sync" {
+  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [
+    kubernetes_namespace.flux_system,
+    kubectl_manifest.install
+  ]
+  yaml_body  = each.value
+}
 
-# resource "kubernetes_secret" "main" {
-#   depends_on = [
-#     kubectl_manifest.install
-#   ]
+resource "kubernetes_secret" "main" {
+  depends_on = [
+    kubectl_manifest.install
+  ]
 
-#   metadata {
-#     name      = data.flux_sync.main.secret
-#     namespace = data.flux_sync.main.namespace
-#   }
+  metadata {
+    name      = data.flux_sync.main.secret
+    namespace = data.flux_sync.main.namespace
+  }
 
-#   data = {
-#     identity       = tls_private_key.main.private_key_pem
-#     "identity.pub" = tls_private_key.main.public_key_pem
-#     known_hosts    = local.known_hosts
-#   }
-# }
+  data = {
+    identity       = tls_private_key.main.private_key_pem
+    "identity.pub" = tls_private_key.main.public_key_pem
+    known_hosts    = local.known_hosts
+  }
+}
 
 resource "github_repository_deploy_key" "main" {
   title      = "flux-${var.stage}"
@@ -84,26 +84,26 @@ resource "github_repository_deploy_key" "main" {
   read_only  = true
 }
 
-# resource "github_repository_file" "install" {
-#   repository = var.repository_name
-#   file       = data.flux_install.main.path
-#   content    = data.flux_install.main.content
-#   branch     = var.branch
-#   overwrite_on_create = true
-# }
+resource "github_repository_file" "install" {
+  repository = var.repository_name
+  file       = data.flux_install.main.path
+  content    = data.flux_install.main.content
+  branch     = var.branch
+  overwrite_on_create = true
+}
 
-# resource "github_repository_file" "sync" {
-#   repository = var.repository_name
-#   file       = data.flux_sync.main.path
-#   content    = data.flux_sync.main.content
-#   branch     = var.branch
-#   overwrite_on_create = true
-# }
+resource "github_repository_file" "sync" {
+  repository = var.repository_name
+  file       = data.flux_sync.main.path
+  content    = data.flux_sync.main.content
+  branch     = var.branch
+  overwrite_on_create = true
+}
 
-# resource "github_repository_file" "kustomize" {
-#   repository = var.repository_name
-#   file       = data.flux_sync.main.kustomize_path
-#   content    = data.flux_sync.main.kustomize_content
-#   branch     = var.branch
-#   overwrite_on_create = true
-# }
+resource "github_repository_file" "kustomize" {
+  repository = var.repository_name
+  file       = data.flux_sync.main.kustomize_path
+  content    = data.flux_sync.main.kustomize_content
+  branch     = var.branch
+  overwrite_on_create = true
+}

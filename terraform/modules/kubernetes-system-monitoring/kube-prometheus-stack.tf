a78a3c935899ecc70afba55226e3dcbf9f7508f8
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-01-21 18:03:08 +0100

    used rendered template

--- terraform/modules/kubernetes-system-monitoring/kube-prometheus-stack.tf
+++ terraform/modules/kubernetes-system-monitoring/kube-prometheus-stack.tf
@@ -10,6 +10,19 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
+data "template_file" "this" {
+  template = file("${path.module}/templates/kube-prometheus-stack.values.tpl")
+  vars = {
+    grafana_adminPassword = var.grafana_adminPassword
+    grafana_root_url = var.grafana_root_url
+    grafana_client_id = var.grafana_client_id
+    grafana_client_secret = var.grafana_client_secret
+    grafana_auth_url = var.grafana_auth_url
+    grafana_token_url = var.grafana_token_url
+    grafana_api_url = var.grafana_api_url
+  }
+}
+
 resource "helm_release" "monitoring" {
   name       = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
@@ -19,6 +32,10 @@ resource "helm_release" "monitoring" {
 
   depends_on = [
     kubernetes_namespace.monitoring,
+  ]
+
+  values = [
+    data.template_file.this.rendered
   ]
 }
 

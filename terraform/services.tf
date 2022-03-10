77b47c9be5d54c97dd3d44e260e04fec4208c9b1
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-03-10 15:25:28 +0100

    update variabels and modules

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,51 +1,51 @@
 module "zelos_system_service_cert_manager" {
-  source            = "./modules/kubernetes-system-cert-manager"
+  source = "jakoberpf/certmanager-deployment/kubernetes"
+
   cloudflare_tokens = var.cloudflare_tokens
 }
 
 module "zelos_system_service_traefik" {
-  source = "./modules/kubernetes-system-traefik"
+  source = "jakoberpf/traefik-deployment/kubernetes"
   depends_on = [
     module.zelos_system_service_cert_manager
   ]
 }
 
 module "zelos_system_service_longhorn" {
-  source = "./modules/kubernetes-system-longhorn"
+  source = "jakoberpf/longhorn-deployment/kubernetes"
   depends_on = [
     module.zelos_system_service_traefik
   ]
+
   gatekeeper_client_id       = var.longhorn_gatekeeper_client_id
   gatekeeper_client_secret   = var.longhorn_gatekeeper_client_secret
   gatekeeper_encryption_key  = var.longhorn_gatekeeper_encryption_key
   gatekeeper_redirection_url = var.longhorn_gatekeeper_redirection_url
   gatekeeper_discovery_url   = var.longhorn_gatekeeper_discovery_url
+  cloudflared_certs          = var.cloudflared_certs
 }
 
 module "zelos_system_service_monitoring" {
-  source = "./modules/kubernetes-system-monitoring"
+  source = "jakoberpf/monitoring-deployment/kubernetes"
   depends_on = [
     module.zelos_system_service_traefik
   ]
+
+  grafana_adminPassword = var.grafana_adminPassword
+  grafana_root_url      = var.grafana_root_url
+  grafana_client_id     = var.grafana_client_id
+  grafana_client_secret = var.grafana_client_secret
+  grafana_auth_url      = var.grafana_auth_url
+  grafana_token_url     = var.grafana_token_url
+  grafana_api_url       = var.grafana_api_url
 }
 
 module "zelos_system_service_flux" {
-  source = "./modules/kubernetes-system-flux"
-  depends_on = [
-    module.zelos_system_service_traefik,
-    module.zelos_system_service_longhorn
-  ]
+  source = "jakoberpf/flux-deployment/kubernetes"
+
   github_owner    = var.github_owner
   github_token    = var.github_token
   repository_name = var.repository_name
   branch          = var.branch
   stage           = var.stage
 }
-
-# module "zelos_system_service_argocd" {
-#   source = "./modules/kubernetes-system-argocd"
-#   depends_on = [
-#     module.zelos_system_service_traefik,
-#     module.zelos_system_service_longhorn
-#   ]
-# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,71 @@
+# ChertManager
+variable "cloudflare_tokens" {
+  type = map(object({
+    name   = string
+    host   = string
+    domain = string
+    token  = string
+  }))
+  description = "The API tokens for the cloudflare zones"
+}
+
+# Longhorn
+variable "longhorn_gatekeeper_client_id" {
+  type        = string
+  description = ""
+}
+
+variable "longhorn_gatekeeper_client_secret" {
+  type        = string
+  description = ""
+}
+
+variable "longhorn_gatekeeper_encryption_key" {
+  type        = string
+  description = ""
+}
+
+variable "longhorn_gatekeeper_redirection_url" {
+  type        = string
+  description = ""
+}
+
+variable "longhorn_gatekeeper_discovery_url" {
+  type        = string
+  description = ""
+}
+
+# Monitoring - Grafana
+variable "grafana_adminPassword" {
+  type        = string
+  description = ""
+}
+variable "grafana_root_url" {
+  type        = string
+  description = ""
+}
+variable "grafana_client_id" {
+  type        = string
+  description = ""
+}
+variable "grafana_client_secret" {
+  type        = string
+  description = ""
+}
+variable "grafana_auth_url" {
+  type        = string
+  description = ""
+}
+variable "grafana_token_url" {
+  type        = string
+  description = ""
+}
+variable "grafana_api_url" {
+  type        = string
+  description = ""
+}
+
+# Flux
 variable "github_owner" {
   type        = string
   description = "github owner"
@@ -23,39 +91,4 @@ variable "stage" {
   type        = string
   default     = "live"
   description = "stage [dev|live]"
-}
-
-variable "cloudflare_tokens" {
-  type = map(object({
-          name  = string
-          host = string
-          domain  = string
-          token = string
-         }))
-  description = "The API tokens for the cloudflare zones"
-}
-
-variable "longhorn_gatekeeper_client_id" {
-  type        = string
-  description = ""
-}
-
-variable "longhorn_gatekeeper_client_secret" {
-  type        = string
-  description = ""
-}
-
-variable "longhorn_gatekeeper_encryption_key" {
-  type        = string
-  description = ""
-}
-
-variable "longhorn_gatekeeper_redirection_url" {
-  type        = string
-  description = ""
-}
-
-variable "longhorn_gatekeeper_discovery_url" {
-  type        = string
-  description = ""
 }

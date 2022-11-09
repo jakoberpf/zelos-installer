d22b6d66baffd6c1cb06d4e2c210aa5f89a1a138
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-09 22:35:59 +0100

    update setup secrets

--- bin/secrets-setup.sh
+++ bin/secrets-setup.sh
@@ -2,11 +2,6 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
-# Setup SSH credentials
-mkdir -p .ssh
-echo -e $(yq e -o=j -I=0 '.ssh.private_key' terraform/terragrunt.yaml | tr -d '"') > .ssh/automation
-chmod 600 .ssh/automation
-
 # Setup OCI credentials ...
 mkdir -p .oci
 mkdir -p .oci/keys
@@ -19,7 +14,7 @@ oci_user=$(echo "${oci_credentials}" | yq '.user_ocid')
 oci_fingerprint=$(echo "${oci_credentials}" | yq '.fingerprint')
 oci_tenancy=$(echo "${oci_credentials}" | yq '.tenancy_ocid')
 oci_region=$(echo "${oci_credentials}" | yq '.region')
-printf "[KUBENETES]\nuser=${oci_user}\nfingerprint=${oci_fingerprint}\ntenancy=${oci_tenancy}\nregion=${oci_region}\nkey_file=${PWD}/.oci/keys/kubernetes.pem\n\n" >> .oci/config
+printf "[KUBERNETES]\nuser=${oci_user}\nfingerprint=${oci_fingerprint}\ntenancy=${oci_tenancy}\nregion=${oci_region}\nkey_file=${PWD}/.oci/keys/kubernetes.pem\n\n" >> .oci/config
 echo -e $(echo "${oci_credentials}" | yq '.private_key') > .oci/keys/kubernetes.pem
 oci setup repair-file-permissions --file .oci/config
 oci setup repair-file-permissions --file .oci/keys/kubernetes.pem
@@ -29,5 +24,4 @@ oci_credentials=$(yq e -o=j -I=0 ".oci" terraform/terragrunt.yaml)
 oci_compartment_ocid=$(echo "${oci_credentials}" | yq '.compartment_ocid')
 oci_namespace=$(oci os ns get --profile=KUBERNETES | jq '.data' | xargs)
 oci_bucket=$(oci os bucket list --profile=KUBERNETES --compartment-id="$oci_compartment_ocid" | jq '.data[] | select(.name=="zelos")')
-echo "$oci_bucket"
-# oci_last_modified=$(oci os object get --profile=KUBERNETES -ns "$oci_namespace" -bn zelos --name kubespray/admin.live.conf --file $config_file --force)
+oci os object get --profile=KUBERNETES -ns "$oci_namespace" -bn zelos --name kubespray/admin.live.conf --file .kube/admin.live.conf

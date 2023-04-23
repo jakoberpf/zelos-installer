b792175e1584bc65b52db9d423933cf20d239d25
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-04-23 14:15:36 +0200

    update service definitions

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,35 +1,30 @@
 module "external_secrets" {
-  source = "jakoberpf/external-secrets-deployment/kubernetes"
-
-  compartment = "zelos-installer"
-
-  vault_server = var.vault_server
-  vault_token  = var.vault_token
+  source             = "jakoberpf/external-secrets-deployment/kubernetes"
+  helm_chart_version = "0.7.2"
+  compartment        = "zelos-installer"
+  vault_server       = var.vault_server
+  vault_token        = var.vault_token
 }
 
 module "cert_manager" {
-  source = "jakoberpf/certmanager-deployment/kubernetes"
-
-  compartment = "zelos-installer"
-
-  cloudflare_tokens = var.cloudflare_tokens
+  source                       = "jakoberpf/certmanager-deployment/kubernetes"
+  helm_chart_version_manager   = "1.11.0"
+  helm_chart_version_reflector = "6.1.47"
+  compartment                  = "zelos-installer"
+  cloudflare_tokens            = var.cloudflare_tokens
 }
 
 module "longhorn" {
   depends_on = [
     module.cert_manager
   ]
-
-  source = "jakoberpf/longhorn-deployment/kubernetes"
-
-  compartment = "zelos-installer"
-
-  aws_access_key_id     = var.longhorn_aws_access_key_id
-  aws_secret_access_key = var.longhorn_aws_secret_access_key
-
-  ingress_dns  = var.longhorn_ingress_dns
-  ingress_type = "istio"
-
+  source                     = "jakoberpf/longhorn-deployment/kubernetes"
+  compartment                = "zelos-installer"
+  helm_chart_version         = "1.4.0"
+  aws_access_key_id          = var.longhorn_aws_access_key_id
+  aws_secret_access_key      = var.longhorn_aws_secret_access_key
+  ingress_dns                = var.longhorn_ingress_dns
+  ingress_type               = "istio"
   gatekeeper_client_id       = var.longhorn_gatekeeper_client_id
   gatekeeper_client_secret   = var.longhorn_gatekeeper_client_secret
   gatekeeper_encryption_key  = var.longhorn_gatekeeper_encryption_key
